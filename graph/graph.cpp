cbb727a4fe9506cac06e47014d3dd25e53329f75
Julien Cheny <julien.cheny@yahoo.fr>
2017-04-03 11:56:26 +0200

    initial commit

---
+++ graph/graph.cpp
@@ -0,0 +1,14 @@
+// graph.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+	cout << "hello"<<endl;
+	return 0;
+}
+
